d43ac7df8f80f0b3cae3716bd48c3dbdd8902aa1
Arun Babu Neelicattu <abn@redhat.com>
2013-05-05 15:34:42 +1000

    Moving processing logic from CLI to Scanner class.

---
+++ src/main/java/com/redhat/victims/VictimsScanner.java
@@ -0,0 +1,75 @@
+package com.redhat.victims;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+
+import com.redhat.victims.fingerprint.Artifact;
+import com.redhat.victims.fingerprint.Processor;
+
+public class VictimsScanner {
+	/**
+	 * 
+	 * Scans a provided {@link File} producing {@link VictimsRecord}. The string
+	 * values of the resulting records will be written to the specified output
+	 * stream.
+	 * 
+	 * @param file
+	 * @param os
+	 * @throws IOException
+	 */
+	private static void scanFile(File file, OutputStream os) throws IOException {
+		File f = file;
+		String path = f.getAbsolutePath();
+		Artifact artifact = Processor.process(path);
+		VictimsRecord vr = new VictimsRecord(artifact);
+		String line = vr.toString();
+		line += "\n";
+		os.write(line.getBytes());
+	}
+
+	/**
+	 * 
+	 * Iteratively finds all jar files in a given directory and scans them,
+	 * producing {@link VictimsRecord}. The string values of the resulting
+	 * records will be written to the specified output stream.
+	 * 
+	 * @param dir
+	 * @param os
+	 * @throws IOException
+	 */
+	private static void scanDir(File dir, OutputStream os) throws IOException {
+		Collection<File> files = FileUtils.listFiles(dir, new RegexFileFilter(
+				"^(.*?)\\.jar"), DirectoryFileFilter.DIRECTORY);
+		Iterator<File> fi = files.iterator();
+		while (fi.hasNext()) {
+			scanFile(fi.next(), os);
+		}
+	}
+
+	/**
+	 * 
+	 * Iteratively finds all jar files if source is a directory and scans them
+	 * or if a file , scan it, producing {@link VictimsRecord}. The string
+	 * values of the resulting records will be written to the specified output
+	 * stream.
+	 * 
+	 * @param source
+	 * @param os
+	 * @throws IOException
+	 */
+	public static void scan(String source, OutputStream os) throws IOException {
+		File f = new File(source);
+		if (f.isDirectory()) {
+			scanDir(f, os);
+		} else if (f.isFile()) {
+			scanFile(f, os);
+		}
+	}
+}
