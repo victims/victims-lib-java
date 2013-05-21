01aa48ed941d7644e94358026bbf19e3cf8f05d7
Arun Babu Neelicattu <abn@redhat.com>
2013-05-21 17:25:07 +1000

    Added a db interface

---
+++ src/main/java/com/redhat/victims/database/VictimsDBInterface.java
@@ -0,0 +1,50 @@
+package com.redhat.victims.database;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.redhat.victims.VictimsException;
+import com.redhat.victims.VictimsRecord;
+
+public interface VictimsDBInterface {
+
+	/**
+	 * Returns when the database was successfully updated
+	 * 
+	 * @return A {@link Date} object indicating when the last update was
+	 *         performed
+	 * @throws VictimsException
+	 */
+	public Date lastUpdated() throws VictimsException;
+
+	/**
+	 * Synchronizes the database with the changes fetched from the victi.ms
+	 * service
+	 * 
+	 * @return <code>true</code> if the the sync was successful else
+	 *         <code>false</code>.
+	 * @throws VictimsException
+	 */
+	public boolean synchronize() throws VictimsException;
+
+	/**
+	 * Given a {@link VictimsRecord}, finds all CVEs that the artifact is
+	 * vulnerable to.
+	 * 
+	 * @param vr
+	 * @return
+	 * @throws VictimsException
+	 */
+	public ArrayList<String> getVulnerabilities(VictimsRecord vr)
+			throws VictimsException;
+
+	/**
+	 * 
+	 * @param sha512
+	 * @return
+	 * @throws VictimsException
+	 */
+	public ArrayList<String> getVulnerabilities(String sha512)
+			throws VictimsException;
+
+}
