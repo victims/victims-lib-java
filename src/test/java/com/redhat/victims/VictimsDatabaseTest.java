135dce2779f36bda490217896e3ec010eadb3b58
Arun Babu Neelicattu <abn@redhat.com>
2013-05-22 21:01:26 +1000

    Added test case validating embedded hashes check.

--- src/test/java/com/redhat/victims/VictimsDatabaseTest.java
+++ src/test/java/com/redhat/victims/VictimsDatabaseTest.java
@@ -1,22 +1,31 @@
 package com.redhat.victims;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashSet;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.redhat.victims.VictimsService.RecordStream;
 import com.redhat.victims.database.VictimsDB;
 import com.redhat.victims.database.VictimsDBInterface;
+import com.redhat.victims.fingerprint.Algorithms;
 import com.redhat.victims.mock.MockEnvironment;
 
 public class VictimsDatabaseTest {
+	private static final String TEST_RESPONSE = "testdata/service/test.response";
 	private static final String TEST_SHA512 = "testdata/service/test.sha512";
 	private static final String TEST_CVE = "testdata/service/test.cve";
+
+	private VictimsDBInterface vdb;
 
 	@BeforeClass
 	public static void setUp() throws IOException {
@@ -28,14 +37,46 @@ public class VictimsDatabaseTest {
 		MockEnvironment.tearDown();
 	}
 
+	@Before
+	public void setVDB() throws VictimsException {
+		vdb = VictimsDB.db();
+		vdb.synchronize();
+	}
+
 	@Test
 	public void testSynchronize() throws VictimsException, IOException {
-		VictimsDBInterface vdb = VictimsDB.db();
-		vdb.synchronize();
 		String sha512 = FileUtils.readFileToString(new File(TEST_SHA512))
 				.trim();
 		String cve = FileUtils.readFileToString(new File(TEST_CVE)).trim();
 		assertTrue("Synchronized DB does not contain expected hash.", vdb
 				.getVulnerabilities(sha512).contains(cve));
+	}
+
+	private HashSet<String> getVulnerabilities(VictimsRecord vr)
+			throws VictimsException {
+		return vdb.getVulnerabilities(vr);
+	}
+
+	@Test
+	public void testVulnerabilities() throws IOException, VictimsException {
+		FileInputStream fin = new FileInputStream(TEST_RESPONSE);
+		RecordStream rs = new RecordStream(fin);
+		VictimsRecord vr;
+		while (rs.hasNext()) {
+			vr = rs.getNext();
+			HashSet<String> cves = getVulnerabilities(vr);
+			vr.hash = "0";
+			if (vr.getHashes(Algorithms.SHA512).size() > 0) {
+				HashSet<String> result = getVulnerabilities(vr);
+				assertEquals("Unexpected number of CVEs", cves.size(),
+						result.size());
+				for (String cve : cves) {
+					assertTrue(String.format(
+							"%s was expected, but was not found in result.",
+							cve), result.contains(cve));
+				}
+				break;
+			}
+		}
 	}
 }
