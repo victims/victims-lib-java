c79e65363078c5f1b80a682b43e64e2502c30986
Arun Babu Neelicattu <abn@redhat.com>
2013-05-22 19:54:45 +1000

    Added VictimsDatabaseTest

---
+++ src/test/java/com/redhat/victims/VictimsDatabaseTest.java
@@ -0,0 +1,37 @@
+package com.redhat.victims;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.redhat.victims.database.VictimsDB;
+import com.redhat.victims.database.VictimsDBInterface;
+import com.redhat.victims.mock.MockEnvironment;
+
+public class VictimsDatabaseTest {
+	private static final String TEST_SHA512 = "testdata/service/test.sha512";
+	private static final String TEST_CVE = "testdata/service/test.cve";
+	
+	@BeforeClass
+	public static void setUp() throws IOException {
+		MockEnvironment.setUp();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		MockEnvironment.tearDown();
+	}
+	
+	@Test
+	public void testSynchronize() throws VictimsException, IOException {
+		VictimsDBInterface vdb = VictimsDB.db();
+		vdb.synchronize();
+		String sha512 = FileUtils.readFileToString(new File(TEST_SHA512)).trim();
+		String cve = FileUtils.readFileToString(new File(TEST_CVE)).trim();
+		assert vdb.getVulnerabilities(sha512).contains(cve);
+	}
+}
