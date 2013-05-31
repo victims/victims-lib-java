71198be12e6013f21d6053f2fb53aede99dbcbf6
Arun Babu Neelicattu <abn@redhat.com>
2013-05-31 21:31:52 +1000

    Renamed hash to key in the cache implementation. Added some documentation.

--- src/main/java/com/redhat/victims/VictimsResultCache.java
+++ src/main/java/com/redhat/victims/VictimsResultCache.java
@@ -10,15 +10,33 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * An implementation facilitating victims scan results. Allows for mapping a key
+ * (eg: sha512) to comma separated list of CVEs.
+ * 
+ * @author abn
+ * 
+ */
 public class VictimsResultCache {
 	protected static String CACHE_NAME = "lib.results.cache";
 	protected static String location = null;
 	protected static boolean purged = false;
 
+	/**
+	 * Create the parent caching directory.
+	 * 
+	 * @param cache
+	 * @throws IOException
+	 */
 	protected void create(File cache) throws IOException {
 		FileUtils.forceMkdir(cache);
 	}
 
+	/**
+	 * Purge the cache. The cache directory is removed and re-recreated.
+	 * 
+	 * @throws IOException
+	 */
 	public void purge() throws IOException {
 		File cache = new File(location);
 		if (cache.exists()) {
@@ -35,21 +53,43 @@ public class VictimsResultCache {
 			create(cache);
 		} else if (VictimsConfig.purgeCache() && !purged) {
 			purge();
+			// make sure that another instance does not purge again
 			purged = true;
 		}
 	}
 
-	public boolean exists(String hash) {
-		return FileUtils.getFile(location, hash).exists();
+	/**
+	 * Test if the given key is cached.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean exists(String key) {
+		return FileUtils.getFile(location, key).exists();
 	}
 
-	public void delete(String hash) throws IOException {
-		FileUtils.forceDelete(FileUtils.getFile(location, hash));
+	/**
+	 * Delete the cache entry for a given key.
+	 * 
+	 * @param key
+	 * @throws IOException
+	 */
+	public void delete(String key) throws IOException {
+		FileUtils.forceDelete(FileUtils.getFile(location, key));
 	}
 
-	public void add(String hash, Collection<String> cves) throws IOException {
-		if (exists(hash)) {
-			delete(hash);
+	/**
+	 * Add a new cache entry.
+	 * 
+	 * @param key
+	 * @param cves
+	 *            A {@link Collection} of CVE Strings. If null an empty string
+	 *            is used.
+	 * @throws IOException
+	 */
+	public void add(String key, Collection<String> cves) throws IOException {
+		if (exists(key)) {
+			delete(key);
 		}
 
 		String result = "";
@@ -58,23 +98,29 @@ public class VictimsResultCache {
 		}
 
 		FileOutputStream fos = new FileOutputStream(FileUtils.getFile(location,
-				hash));
+				key));
 		try {
-			fos.write(result.getBytes());			
+			fos.write(result.getBytes());
 		} finally {
-			fos.close();			
+			fos.close();
 		}
 
 	}
 
-	public HashSet<String> get(String hash) throws IOException {
+	/**
+	 * Get the CVEs mapped by a key
+	 * 
+	 * @param key
+	 * @return {@link Collection} of CVE strings
+	 * @throws IOException
+	 */
+	public HashSet<String> get(String key) throws IOException {
 		String result = FileUtils.readFileToString(
-				FileUtils.getFile(location, hash)).trim();
+				FileUtils.getFile(location, key)).trim();
 		HashSet<String> cves = new HashSet<String>();
-		for(String cve : StringUtils.split(result, ",")) {
+		for (String cve : StringUtils.split(result, ",")) {
 			cves.add(cve);
 		}
 		return cves;
 	}
-
 }
