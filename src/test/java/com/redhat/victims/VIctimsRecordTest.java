9ffcb034475ab7ec242ecdbc9975dbc36902b638
Arun Babu Neelicattu <abn@redhat.com>
2013-06-03 20:23:41 +1000

    Added test case for VictimsRecord equality

---
+++ src/test/java/com/redhat/victims/VIctimsRecordTest.java
@@ -0,0 +1,20 @@
+package com.redhat.victims;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class VIctimsRecordTest {
+	@Test
+	public void testEquals() throws IOException {
+		String jstr = FileUtils.readFileToString(
+				new File(Resources.JAR_JSON)).trim();
+		VictimsRecord vr = VictimsRecord.fromJSON(jstr);
+		assertTrue("Equality check for Victims Record failed.", vr.equals(vr));
+	}
+
+}
