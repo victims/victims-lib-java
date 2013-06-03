d97c9a8b4be19eba082753e62f078c524ca4e0a6
Arun Babu Neelicattu <abn@redhat.com>
2013-06-03 20:22:03 +1000

    Added Resources class for test package

---
+++ src/test/java/com/redhat/victims/Resources.java
@@ -0,0 +1,15 @@
+package com.redhat.victims;
+
+public class Resources {
+	public static final String JAR_FILE = "testdata/junit-4.11/junit-4.11.jar";
+	public static final String JAR_SHA1 = JAR_FILE + ".sha1";
+	public static final String JAR_JSON = JAR_FILE + ".json";
+	public static final String POM_FILE = "testdata/junit-4.11/junit-4.11.pom";
+	public static final String POM_SHA1 = POM_FILE + ".sha1";
+	public static final String POM_JSON = POM_FILE + ".json";
+	
+	public static final String TEST_RESPONSE = "testdata/service/test.response";
+	public static final String TEST_SHA512 = "testdata/service/test.sha512";
+	public static final String TEST_CVE = "testdata/service/test.cve";
+	
+}
