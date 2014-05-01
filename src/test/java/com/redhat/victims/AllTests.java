f4891c2f36a502628f735d2415923ac9fb913121
Arun Babu Neelicattu <abn@redhat.com>
2014-05-02 01:02:21 +1000

    Formatting fix

--- src/test/java/com/redhat/victims/AllTests.java
+++ src/test/java/com/redhat/victims/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ OfflineTests.class, OnlineTests.class})
+@SuiteClasses({ OfflineTests.class, OnlineTests.class })
 public class AllTests {
 
 }
